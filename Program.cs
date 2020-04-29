49f6f2abb360b9432770979bb68fdcea763131cd
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2020-04-29 21:34:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "13";
+
+            int num1 = int.Parse(myString);
+            int num2 = int.Parse(mySecondString);
+
+            int resultInt = num1 + num2;
+
+            string result = myString + mySecondString;  // ergebnis : 1513 
+            Console.WriteLine(resultInt);
+            Console.ReadKey();
+        }
+    }
+}
