af89f8b5c85bf95468340ceb6a915f3be721f9ea
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2020-04-29 21:36:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VoidMethode
+{
+    class Program
+    {
+        // Startpunkt des Programms
+
+        static void Main(string[] args)
+        {
+            WriteSomething();  //aufrufen um die Methode unten (Zeile 15-19) auszuführen
+            WriteSomething();
+            WriteSomething();
+            string myArgument = "Hallo ich bin super Text";
+            WriteSomethingSpecific(myArgument);
+
+            Console.ReadKey();
+        }
+        // wie eine Methode aufgebaut ist : 
+        // Zugriffmodifizierer (static) Rückgabetyp Methodenname(parameter1, parameter2) 
+        public static void WriteSomething()
+        {
+            Console.WriteLine("Ich bin eine Methode");
+
+        }
+
+        public static void WriteSomethingSpecific(string myText)  // "string myText" ist ein Parameter
+        {
+        
+           Console.WriteLine(myText);
+        }
+    }
+
+}
+
