b86ff9c7c98f45cf615d0aa73402e0ec8535a58d
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2020-04-29 21:40:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace IfBedingungenEinführung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie warm ist es gerade ? Trage den Wert als Zahl ein " );
+            // Benutzereingabe
+            string temperatureInput = Console.ReadLine();
+            // int temperature = int.Parse(temperatureInput);
+            int temperature;
+            int number;
+            if (int.TryParse(temperatureInput, out number))  // out = output (Ausgabe)
+              // oder bool userEnter = int.TryParse(temperatureInput, out number); & dann if (userEnter)
+            {
+                temperature = number; 
+            }
+            else
+            {
+                temperature = 0;
+            }
+
+            if(temperature < 20)
+            {
+                Console.WriteLine("Ziehe deine Jacke an ");
+            }
+
+            else if (temperature == 20) 
+            {
+               
+                Console.WriteLine("Es ist genau 20° C warm");
+            }
+            else if(temperature > 30)
+            {
+                Console.WriteLine("es ist eine Bullenhitze");
+            }
+            else  // ( else = ansonsten )
+            {
+                Console.WriteLine( "Zwischen 21 und 30 Grad - T-shirt und kurze Hose reicht ! ");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
