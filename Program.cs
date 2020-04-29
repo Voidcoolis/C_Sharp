98bd92fea280ef51153aee1ee3ea9780609b4fff
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2020-04-29 21:37:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anfang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
