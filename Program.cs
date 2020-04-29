1d7456c5a2a1ef7a36b8d653330dae32833b5ff7
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2020-04-29 21:33:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Namensgebung
+{
+    // Klassennamen wie z.B ClientActivity
+    class Program
+    {
+        // Methodenname wie z.B CalculateValue
+        // Methodenargumente werden klein geschrieben
+        static void Main(string[] args)
+        {
+
+            // variable heißt wie z.B itemCount
+            // nutze userControl statt useCtr
+
+
+            // keine Zahlen bei Variablennamen am Anfang verwenden
+            int num5 = 3;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
