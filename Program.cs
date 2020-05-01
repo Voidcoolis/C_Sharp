108e61265a3f255a70efdd1d2cf76399893269a3
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2020-05-02 01:55:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int counter = 0;
+            
+            while (counter <10)    
+            {
+                Console.WriteLine(counter);   // shkon nga 0 deri ne 9 
+                counter++;  // damit nicht endlos wird
+
+            }
+            */
+
+
+            int counter = 0;
+            string enteredText = "";
+            // hier wird überprüft ob enteredText leerer String ist
+            while (enteredText.Equals("") )    
+            {
+                Console.WriteLine("Bitte drücke Enter um eins hochzuzählen, oder gib was anderes ein, um die Zählung zu beenden");
+                // Benutzereingabe
+                enteredText = Console.ReadLine();
+                Console.WriteLine("Aktuelle Anzahl der Schüler im bus {0}", counter);
+                // zählen hich
+                counter++;
+            }
+            Console.WriteLine("{0} Anzahl Schüler sind im Bus.Es kann losgehen.Drücke Enter um loszufahren",counter);
+
+            Console.ReadKey();
+        }
+    }
+}
+                // Das ist ein Program um Schüler oder was anderes zu zählen
