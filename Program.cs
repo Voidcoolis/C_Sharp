2b1dd0626a0aeadf599d61de35d940069d369686
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2021-05-04 22:12:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, i, m = 0, flag = 0;
+            Console.Write("Enter the Number to check Prime: ");
+
+            n = int.Parse(Console.ReadLine());
+            m = n/ 2;
+
+            for(i=2; i<=m; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.Write("The Number is not a PrimeNumber ! ");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.Write("The Number is a PrimeNumber ! ");
+
+        }
+    }
+}
