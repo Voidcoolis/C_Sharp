2bcf1b59258ac6720c014fb1a2eccba8a103b449
Jovi-debug <64271963+Jovi-debug@users.noreply.github.com>
2021-08-17 11:55:52 +0200

    Add files via upload

---
+++ ASCIIValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ASCIIValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My lord please write something : ");
+
+            string str = Console.ReadLine();
+         
+            foreach (var value in str)
+            {
+                Console.WriteLine((int)value);
+            }
+
+
+            //Console.WriteLine("My lord please write something again: ");
+            
+            //Convert.ToChar(Console.ReadLine());
+            
+        }
+    }
+}
